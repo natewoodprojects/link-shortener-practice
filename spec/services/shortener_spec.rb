fdf19700cf56494e5c0cbcf6d09db200298282ca
Nate Wood <99522059+natewoodprojects@users.noreply.github.com>
2022-09-07 13:27:11 -0500

    initial commit

---
+++ spec/services/shortener_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe Shortener do
+
+    it "shortens a given url to a seven character lookup code" do
+        url = "https://www.codewars.com/users/TakoKing8217"
+        shortener = Shortener.new(url)
+        expect(shortener.lookup_code.length).to eq(7)
+    end
+
+    it "gives each url it's own url" do
+        url = "https://www.codewars.com/users/TakoKing8217"
+        shortener = Shortener.new(url)
+        code_1 = shortener.lookup_code
+        expect(shortener.lookup_code.length).to eq(7)
+
+        url = "https://www.codewars.com/users/MichaelWSmith"
+        shortener = Shortener.new(url)
+        code_2 = shortener.lookup_code
+
+        expect(code_2).not_to eq(code_1)
+    end
+    
+    it "it always gives the same URl the same lookup code" do
+        url = "https://www.codewars.com/users/TakoKing8217"
+        shortener = Shortener.new(url)
+        code_1 = shortener.lookup_code
+        expect(shortener.lookup_code.length).to eq(7)
+
+        url = "https://www.codewars.com/users/TakoKing8217"
+        shortener = Shortener.new(url)
+        code_2 = shortener.lookup_code
+
+        expect(code_2).to eq(code_1)
+    end
+end
